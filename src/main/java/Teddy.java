105944b4915db8dd1a9793229b38600585fcaef2
jaylontan <156635750+jaylontan@users.noreply.github.com>
2025-01-21 17:41:11 +0800

    Added ability to input text and the chatbot repeating said input text

--- src/main/java/Teddy.java
+++ src/main/java/Teddy.java
@@ -1,10 +1,13 @@
+import java.util.Scanner;
+
 public class Teddy {
     public static void main(String[] args) {
         String seperator = "_".repeat(60);
         System.out.println(seperator);
         System.out.println("Hello! I'm Teddy\nWhat can I do for you?");
         System.out.println(seperator);
-        System.out.println("Bye. Hope to see you again!");
-        System.out.println(seperator);
+        Scanner sc = new Scanner(System.in);
+        String response = sc.nextLine();
+        System.out.println(response);
     }
 }
