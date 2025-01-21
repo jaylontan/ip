7fb3bef70bb378b41618152d564112016427897b
jaylontan <156635750+jaylontan@users.noreply.github.com>
2025-01-21 17:21:33 +0800

    Revert "Add greeting and exit"
    This reverts commit b75b4caacd31f3bc6f1e02aadd9e5813932ffc93.

--- src/main/java/Teddy.java
+++ src/main/java/Teddy.java
@@ -1,10 +1,10 @@
 public class Teddy {
     public static void main(String[] args) {
-        String seperator = "_".repeat(60);
-        System.out.println(seperator);
-        System.out.println("Hello! I'm Teddy \nWhat can I do for you?");
-        System.out.println(seperator);
-        System.out.println("Bye! Hope to see you again soon!");
-        System.out.println(seperator);
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello! I'm Teddy");
     }
 }
