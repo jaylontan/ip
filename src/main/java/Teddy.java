9dfc9523fef191a4b1bcfe3536e0cb9479200f84
jaylontan <156635750+jaylontan@users.noreply.github.com>
2025-01-21 17:53:33 +0800

    chatbot terminates operation when "bye" is input by user; improved styling of echo as well

--- src/main/java/Teddy.java
+++ src/main/java/Teddy.java
@@ -2,12 +2,21 @@ import java.util.Scanner;
 
 public class Teddy {
     public static void main(String[] args) {
+        // chatbot greeting
         String seperator = "_".repeat(60);
-        System.out.println(seperator);
-        System.out.println("Hello! I'm Teddy\nWhat can I do for you?");
-        System.out.println(seperator);
+        System.out.println(seperator + "\nHello! I'm Teddy\nWhat can I do for you?\n" + seperator);
+
+        // reading input from user
         Scanner sc = new Scanner(System.in);
-        String response = sc.nextLine();
-        System.out.println(response);
+
+        while (true) {
+            String input = sc.nextLine();
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println(seperator + "\nBye! Hope to see you again soon!\n" + seperator);
+                break;
+            }
+
+            System.out.println(seperator + "\n" + input + "\n" + seperator);
+        }
     }
 }
